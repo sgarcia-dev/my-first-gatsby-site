7dd085f03710f025c32bbed48aa91f9d8f8fac75
Sergei Garcia <sgarcia.dev@gmail.com>
2021-12-29 00:12:49 -0600

    tutorial part 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My First Gatsby Site",
+    title: "My First Gatsby Site V3",
+    description: "This is an auto-generated description",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-schema-export",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
