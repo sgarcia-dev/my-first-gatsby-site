9634dbe5c442a6853a8eec6d8cec414b32799ac1
Sergei Garcia <sgarcia.dev@gmail.com>
2022-01-12 02:55:13 -0600

    add tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        className="absolute l-0 t-0"
         src="../images/dog.jpeg"
       />
     </Layout>
